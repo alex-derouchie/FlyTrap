9204890b77142e569d75ef9b686c256d1e95f3ee
Alex Derouchie <alex.derouchie@gmail.com>
2020-07-20 15:45:00 -0400

    Implemented vertical sidebar navigation.

--- FlyTrap/wwwroot/js/site.js
+++ FlyTrap/wwwroot/js/site.js
@@ -1,3 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+$(document).ready(function () {
 
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
